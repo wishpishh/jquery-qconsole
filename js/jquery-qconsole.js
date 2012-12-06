59a09576da71eff0ab15781e8fefe434da92b2cc
Hannes Johansson <wishpishh@gmail.com>
2012-12-06 15:52:45 +0100

    added support some native commands

--- js/jquery-qconsole.js
+++ js/jquery-qconsole.js
@@ -1,11 +1,44 @@
 (function($) {
 	"use strict";
 	
-	var defaults = {
+	var settings = {
 		dropdownDuration: 150,
 		historySize: 50,
 		triggerKeyCombos: [[17, 188]] // ctrl + ,
 	};	
+	
+	var nativeCommands = {
+		help: {
+			helptext: 'Print list of commands and their help texts',
+			command: function() {
+				var retVal = '<span class="qc-output">Available commands:</span>';
+			
+				for (var c in commandList) {
+					retVal += '<span class="qc-output qc-tab">' + c + ':';
+					if (commandList[c].helptext) {
+						retVal += '<span class="qc-output qc-tab">' + commandList[c].helptext + '</span>';
+					}
+					retVal += '</span>';
+				}
+			
+				return retVal;
+			}
+		},
+		clear: {
+			helptext: 'Clear the display',
+			command: function() {
+				$textarea.empty();
+			}
+		},
+		echo: {
+			helptext: 'Echo the entered text',
+			command: function(val) {
+				return "-> " + val;
+			}
+		}
+	};
+	
+	var commandList = {};
 	
 	var supportsLocalStorage;
 	var activeKeys = [];
@@ -14,7 +47,7 @@
 	var $input, $wrapper, $console, $textarea;
 	
 	$.qconsole = function(options) {
-		var opts = $.extend({}, defaults, options);
+		var opts = $.extend({}, settings, options);
 		
 		// Init qconsole markup
 		var $container = $('<div class="qc-wrapper"><div class="qc-console"><div class="qc-textarea"></div><input class="qc-input" type="text"></input></div></div>');
@@ -26,11 +59,7 @@
 		$textarea = $('.qc-textarea');
 		
 		// Init event handlers
-		$(document).keydown(function(e) {
-			handleGlobalKeydown(opts, e);
-		}).keyup(function(e) {
-			handleGlobalKeyup(opts, e);
-		});		
+		$(document).keydown(handleGlobalKeydown).keyup(handleGlobalKeyup);
 
 		$input.focusin(function(e) {
 			$(this).fadeTo(opts.dropdownDuration, 0.3);
@@ -38,7 +67,7 @@
 			$(this).fadeTo(opts.dropdownDuration, 0.2);
 		}).keyup(function(e) {
 			if(e.which === 13) {
-				handleInput.call(this, opts);
+				handleInput.call(this);
 			} else if (e.which === 38) {
 				
 				if (histCursor > 0) {
@@ -59,18 +88,20 @@
 		supportsLocalStorage = supportsLocalStorage();
 		initHistory();
 	
+		// init commands list
+		$.extend(commandList, nativeCommands);
 	};
 	
-	function handleGlobalKeydown(opts, e) {
+	function handleGlobalKeydown(e) {
 		activeKeys[e.which] = true;
 	};
 	
-	function handleGlobalKeyup(opts, e) {
+	function handleGlobalKeyup(e) {
 		var triggerDropdown = true;
 		var triggerKeyCombo;
 		
-		for (var i in opts.triggerKeyCombos) {
-			triggerKeyCombo = opts.triggerKeyCombos[i];
+		for (var i in settings.triggerKeyCombos) {
+			triggerKeyCombo = settings.triggerKeyCombos[i];
 			for(var j in triggerKeyCombo) {
 				if (!activeKeys[triggerKeyCombo[j]]) {
 					triggerDropdown = false;
@@ -87,27 +118,28 @@
 		activeKeys[e.which] = null;
 	};
 	
-	function handleInput(opts) {
-		var val = $(this).val();
+	function handleInput() {
+		var input = $(this).val();
+		var retVal, $retValWrapper = $('<span class="qc-output"></span>');
 		
-		if (!val) return;
+		if (!input) return;
+		
+		var parsedInput = input.split(' ');
+		var command = parsedInput[0];
+		var args = parsedInput.slice(1, parsedInput.length);
 		
 		$(this).val('');
 		
-		$('<span>' + val + '</span><br/>').appendTo($textarea);
-				
-		if (hist.length >= opts.historySize) {
-			hist.splice(0, 1);
-		}
-
-		if (histCursor !== hist.length && val === hist[histCursor]) {
-			hist.splice(histCursor, 1);
+		if (commandList[command]) {
+			retVal = commandList[command].command.call(this, args);
+		} else {
+			retVal = 'unknown command: ' + input;
+			$retValWrapper.addClass('qc-unknown-command');
 		}
 		
-		hist.push(val);
-		histCursor = hist.length;
+		$retValWrapper.html(retVal).appendTo($textarea);
 		
-		storeHistory();
+		updateHistory(input);
 	};
 	
 	function initHistory() {
@@ -126,13 +158,33 @@
 		}
 	};
 	
+	function updateHistory(input) {
+		if (hist.length >= settings.historySize) {
+			hist.splice(0, 1);
+		}
+
+		if (histCursor !== hist.length && input === hist[histCursor]) {
+			hist.splice(histCursor, 1);
+		}
+		
+		hist.push(input);
+		histCursor = hist.length;
+		
+		storeHistory();
+	};
+	
 	function supportsLocalStorage() {
 		try {
 			return 'localStorage' in window && window['localStorage'] !== null;
 		} catch (e) {
 			return false;
 		}
-	}
+	};
 	
-	$.qconsole.defaults = defaults;
+	function parseParamNames(fn) {
+		var fnStr = commandList[c].toString();
+		return fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(/([^\s,]+)/g);
+	};
+	
+	$.qconsole.settings = settings;
 }(jQuery));
